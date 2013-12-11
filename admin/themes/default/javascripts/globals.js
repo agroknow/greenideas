fabf4e13937d5d59afd455258961e5d38bf36742
gkista <gkista@agroknow.gr>
2013-12-11 23:11:16 +0200

    [configs] justufy in tinymce, styling add bullets in description

--- admin/themes/default/javascripts/globals.js
+++ admin/themes/default/javascripts/globals.js
@@ -23,7 +23,7 @@ Omeka.wysiwyg = function (params) {
         fix_content_duplication: false,
         fix_list_elements: true,
         valid_child_elements: "ul[li],ol[li]",
-        theme_advanced_buttons1: "bold,italic,underline,justifyleft,justifycenter,justifyright,bullist,numlist,link,formatselect,code",
+        theme_advanced_buttons1: "bold,italic,underline,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,link,formatselect,code",
         theme_advanced_buttons2: "",
         theme_advanced_buttons3: "",
         theme_advanced_toolbar_align: "left"
